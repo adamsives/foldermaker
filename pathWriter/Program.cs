14d946d625ffcd9232f789863daf99404bd9dea2
DESKTOP-LHRQ2R4\adamsives <adamsives@gmail.com>
2018-09-17 11:05:49 +1000

    Working C# solution. This contains the C# code to write the folder. It's an intermediatary step before a more complete project (deployed as a Windows a service).

--- pathWriter/Program.cs
+++ pathWriter/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,14 +9,20 @@ namespace pathWriter
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+            pathStringBuilder path = new pathStringBuilder();
+
+            Console.WriteLine(path.BuildFullPath());
+            Console.WriteLine(path.BuildFolderName());
+
             Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            Directory.CreateDirectory(path.BuildFullPath());
+
+    
+
         }
     }
 }
---
+++ pathWriter/pathStringBuilder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pathWriter
+{
+    class pathStringBuilder
+    {
+        string myDocsPath = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\";
+        private static DateTime now = DateTime.Now;
+        readonly string dayNumber = now.Day.ToString().PadLeft(2, '0');
+        readonly string monthName = System.Globalization.CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(now.Month);
+        readonly string monthNumber = now.Month.ToString().PadLeft(2, '0');
+
+        public String BuildFolderName()
+        {
+            string folderName = dayNumber + monthNumber + now.Year;
+            return folderName;
+        }
+
+        public String BuildFullPath()
+        {
+            //string pathAndFolder = myDocsPath + now.Year + "\\" + monthNumber + " " + monthName + "\\" + dayNumber + monthNumber + now.Year;
+            string pathAndFolder = myDocsPath + now.Year + "\\" + monthNumber + " " + monthName + "\\" + BuildFolderName();
+            return pathAndFolder;
+        }
+
+    }
+}
